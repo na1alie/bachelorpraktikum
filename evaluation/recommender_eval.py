7a6f6f3096819a51db802dfafc4d4078abecf818
Natalie Sturm <ge94say@mytum.de>
2025-07-08 14:53:38 +0200

    job evaluation

--- evaluation/recommender_eval.py
+++ evaluation/recommender_eval.py
@@ -3,7 +3,7 @@ import os
 import json
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from ui.recommender_helper import recommend_courses_semantic
+from ui.recommender_helper import recommend_courses_top_coverage, recommend_courses_top_similarity
 
 
 COURSE_JSON = '/home/natalie/Bachelorprojekt/evaluation/course_subset.json'
@@ -19,21 +19,49 @@ subset_names = set(
 
 print(f"Subset courses loaded: {len(subset_names)}")
 
+print("######### TOP SIMILARITY ##########")
+
 # Get recommendations
-top_courses = recommend_courses_semantic(
-    "Programmer - Game Development + Web Front End.",
+top_courses = recommend_courses_top_similarity(
+    "IT Security Engineer",
+    ["Entry level"],
     ["Englisch", "Deutsch/Englisch", "Deutsch"],
     ["Bachelor", "Bachelor/Master", "Master"]
 )
+num_recommended = sum(len(course_list) for _, course_list in top_courses)
+print(f"\nTotal courses recommended: {num_recommended}")
 
 print("\nCourses recommended AND in subset:\n")
-
 matches = []
 
 for job_title, course_list in top_courses:
-    for course_name, course_score, required_skills in course_list:
+    for course_name, _, _ in course_list:
         if course_name in subset_names:
-            print(f"{course_name}: score {course_score:.4f} — teaches: {required_skills}")
+            print(course_name)
+            matches.append(course_name)
+
+if not matches:
+    print("No overlap found.")
+
+
+print("######### TOP COUVERAGE ##########")
+
+top_courses = recommend_courses_top_coverage(
+    "IT Security Engineer",
+    ["Entry level"],
+    ["Englisch", "Deutsch/Englisch", "Deutsch"],
+    ["Bachelor", "Bachelor/Master", "Master"]
+)
+num_recommended = sum(len(course_list) for _, course_list in top_courses)
+print(f"\nTotal courses recommended: {num_recommended}")
+
+print("\nCourses recommended AND in subset:\n")
+matches = []
+
+for job_title, course_list in top_courses:
+    for course_name, _, _ in course_list:
+        if course_name in subset_names:
+            print(course_name)
             matches.append(course_name)
 
 if not matches:
